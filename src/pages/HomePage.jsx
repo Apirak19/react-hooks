7247a83a358c834e43479d92de736c9029d75ace
Apirak Fakin <apirakfakin@gmail.com>
2023-10-29 14:51:53 +0700

    Cover Images finished

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,54 @@
-import React from "react";
-import {Navbar} from "../components/Navbar.jsx";
+import React, { useEffect, useState } from "react";
+import { Navbar } from "../components/Navbar.jsx";
+
+const coverCount = 10;
+const MIN = 1;
 
 export const HomePage = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [transitionDuration, setTransitionDuration] = useState(1);
+  const getCoverImage = (url, initialValue = null) => {
+    const [result, setResult] = useState(initialValue);
+    useEffect(() => {
+      fetch(url).then(async (res) => {
+        const json = await res.json();
+        setResult(json);
+      });
+    }, []);
+    return result;
+  };
+  useEffect(() => {
+    const duration = transitionDuration < MIN ? MIN : transitionDuration;
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % 10);
+    }, duration * 1000);
+
+    return () => {
+      console.log("clear");
+      clearInterval(interval);
+    };
+  }, [transitionDuration]);
+
+  const coverImage = getCoverImage(
+    `https://shibe.online/api/shibes?count=${coverCount}`,
+    []
+  );
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <h1>Welcome to my homepage</h1>
       {/*  TODO: Cover Images */}
+      <h1>Set Duration</h1>
+      <input
+        type="number"
+        value={transitionDuration}
+        onChange={(e) => setTransitionDuration(e.target.value)}
+      />
+
+      <div>
+        <img height={200} src={coverImage[currentIndex]} alt="" />
+      </div>
       {/*  TODO: Jokes */}
     </>
-  )
-}
+  );
+};
